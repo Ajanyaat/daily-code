f560f43ea2d6b6a6cf63002e1030d57c883946c5
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-04-14 15:55:12 +0530

    Add files via upload

---
+++ day2/difference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Enter the value");
+            n=int.Parse(Console.ReadLine());
+            Console.WriteLine(difference(n,51));
+
+        }
+        static int difference(int n, int K)
+        {
+
+
+
+            if (n > K)
+            {
+
+                return (n - K) * 3;
+
+
+            }
+
+            else
+            {
+                return n;
+            }
+           
+          
+
+
+         }
+        
+    }
+}
