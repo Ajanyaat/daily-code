0296a86970f0d2161947926cc017845c130a3cde
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-04-25 15:47:50 +0530

    Add files via upload

---
+++ day11/Mail folder2/MailFolder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mail_folder2
+{
+    internal class MailFolder
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+
+        }
+        private List<Mail> _mailList;
+        public List<Mail> MailList
+        {
+            get
+            { return _mailList; }
+            set { _mailList = value; }
+
+        }
+        public MailFolder()
+        {
+
+        }
+        public MailFolder(string _name, List<Mail> _mailList)
+        {
+
+            Name = _name;
+            MailList = _mailList;
+        }
+        public void AddMailToFolder(Mail mail)
+        {
+            MailList.Add(mail);// add mail to the maillist
+
+        }
+
+        public bool RemoveMailFromFolder(long id)
+        {
+            int count = 0;
+            foreach (var item in MailList) // iterating n0 1 to lat
+            {
+                if (item.Id == id)
+                {
+                    MailList.Remove(item);
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 0)
+            {
+                return false;
+
+            }
+            else
+                return true;
+        }
+        public void DisplayMails()
+        {
+
+            if (MailList.Count > 0)
+            {
+                foreach (var item in MailList)
+                {
+                    Console.WriteLine("mail{0}", Name);
+                    Console.WriteLine(item.ToString());
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No mails to show");
+            }
+        }
+
+
+
+    } }       
+
---
+++ day11/Mail folder2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mail_folder2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mail>ls=new List<Mail>();
+            
+            Console.WriteLine("Enter the folder name");
+            string Name=Console.ReadLine();
+            Mail m= new Mail();
+            MailFolder mf = new MailFolder(Name,ls);
+            while(true)
+            {
+                Console.WriteLine("enter a choice\n 1-Addmail\n 2-Remove mail\n 3-Display mail\n 4-Exit");
+                int a=int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("enter the details");
+                        string details = Console.ReadLine();
+                        m = Mail.CreateMail(details);
+                        mf.AddMailToFolder(m);
+                        Console.WriteLine("mail added successfully");
+                        break;
+                    case 2:
+                        Console.WriteLine("enter the id to be deleted");
+                        int id = int.Parse(Console.ReadLine());
+                        mf.RemoveMailFromFolder(id);
+                        break;
+                    case 3:
+                        mf.DisplayMails();
+                        break;
+                }
+                }
+            }
+           
+
+
+            
+               
+            }
+
+        } 
+
