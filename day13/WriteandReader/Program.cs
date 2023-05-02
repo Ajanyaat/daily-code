9cf14ab28a80cd1b3dcb62fcd87acb57664ffa8b
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-05-02 16:51:10 +0530

    Add files via upload

---
+++ day13/WriteandReader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteandReader
+{
+    internal class Program
+    {
+        class WriteandReader
+        {
+
+            static string file = @"D:\\ust demos\example.txt";
+            static void Main()
+            {
+                Write();
+                // Read();
+            }
+            static void Write()
+            {
+                StreamWriter filestream = new StreamWriter(file);
+                filestream.WriteLine("What is this life");
+                filestream.WriteLine("we have no time to nstand and stare");
+                filestream.Close();
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
