8737282e9e91b2aec8ba3131d22c385209d3afd0
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-05-08 09:45:07 +0530

    Add files via upload

---
+++ day7/EVEN OR ODD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVEN_OR_ODD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("ENTER A NUM");
+            a=int.Parse(Console.ReadLine());
+            if (a % 2 == 0) Console.WriteLine("Even");
+            else Console.WriteLine("Odd");
+        }
+    }
+}
