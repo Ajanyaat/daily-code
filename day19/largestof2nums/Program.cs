dafe3cf31357a0305935ba549dd7812b2a5772a9
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-05-08 09:54:44 +0530

    Add files via upload

---
+++ day19/largestof2nums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace largestof2nums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            
+            Console.WriteLine( "enter num1");
+            num1=int.Parse( Console.ReadLine() );
+           
+            if(num1%2==0) { Console.WriteLine("num1 is divisible"); }
+            else { Console.WriteLine("num2 is not divisible"); }
+        }
+    }
+}
---
+++ day19/sumofnumbersindigit/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumofnumbersindigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int sum = 0;
+            int Num;
+            int rev;
+            Console.WriteLine("enter a num");
+            Num = int.Parse(Console.ReadLine());
+            while (Num != 0)
+            {
+                rev = Num % 10;
+                Num = (Num / 10);
+                sum = sum + rev;
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
