11e9ba22ce9064256c9840e26ef74e83795a3fb2
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-06-03 12:48:35 +0530

    Add files via upload

---
+++ day42/practicequestion1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicequestion1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter ea number");
+            n =int.Parse(Console.ReadLine());
+            if (IsPowerOf2(n))
+            {
+                Console.WriteLine($"{ n} is  power of 2");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not  power of 2");
+            }
+           
+        }
+        static bool IsPowerOf2(int n)
+        {
+            if (n <= 0)
+                return false;
+            while(n%2== 0)
+            {
+                n /= 2;
+            }
+            return n == 1;
+
+            
+        }
+
+    }
+}
---
+++ day42/practicequestion2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace practicequestion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the string");
+            string s=Console.ReadLine();
+            string s1 = Regex.Replace(s, @"(?i)\b(\w+)(\s+\1)+\b", "$1");
+            Console.WriteLine(s1);
+        }
+    }
+}
