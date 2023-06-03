00aa88c591cd1fc57eac46f4e712b28307a1b87a
Ajanyaat <129932631+Ajanyaat@users.noreply.github.com>
2023-06-03 14:09:16 +0530

    Add files via upload

---
+++ day43/practicequestion4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicequestion4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter number of elements");
+            n=int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            Console.WriteLine("Enter elements");
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i]);
+
+            }
+        }
+    }
+}
